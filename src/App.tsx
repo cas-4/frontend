03247c919f7f37aafc40091a1bfb9599a8b548d7
LucaPolese <luca.polese@studio.unibo.it>
2024-12-23 17:19:28 +0100

    Update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MainLayout } from './layouts/MainLayout';
+import Login from './pages/Login';
 import PrivateRoute from './components/PrivateRoute';
-import NotFound from './components/NotFound';
-import Logout from './components/Logout';
-import ErrorPage from './components/ErrorPage';
+import NotFound from './pages/NotFound';
+import Logout from './pages/Logout';
+import ErrorPage from './pages/ErrorPage';
+import Alert from './pages/AlertPage';
+import GeoFencePage from './pages/GeoFencePage';
+import Dashboard from './pages/DashboardPage';
 
-export default function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
         <Route path="/error" element={<ErrorPage />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="*" element={<NotFound />} />
+
+        {/* Private Routes */}
+        <Route element={<PrivateRoute />}>
+          <Route element={<MainLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/alert" element={<Alert />} />
+            <Route path="/geofence" element={<GeoFencePage />} />
+          </Route>
+        </Route>
+
       </Routes>
     </Router>
   );
-}
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-// src/index.tsx
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
